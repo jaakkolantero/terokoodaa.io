8212b68edff9defc2073f91948cb2634f83373f9
Tero Jaakkola <tero.jaakkola1@gmail.com>
2017-10-14 10:23:12 +0300

    Reorganize accounts tests and add test for necessary inputs

--- accounts/tests/test_view_signup.py
+++ accounts/tests/test_view_signup.py
@@ -3,9 +3,9 @@ from django.contrib.auth.forms import UserCreationForm
 from django.core.urlresolvers import reverse
 from django.urls import resolve
 from django.test import TestCase
-from .views import signup
 
-from .forms import SignUpForm
+from ..views import signup
+from ..forms import SignUpForm
 
 class SignUpTests(TestCase):
     def setUp(self):
@@ -25,6 +25,16 @@ class SignUpTests(TestCase):
     def test_contains_form(self):
         form = self.response.context.get('form')
         self.assertIsInstance(form, SignUpForm)
+
+    def test_form_inputs(self):
+        '''
+        The view must contain five inputs: csrf, username, email,
+        password1, password2
+        '''
+        self.assertContains(self.response, '<input', 5)
+        self.assertContains(self.response, 'type="text"', 1)
+        self.assertContains(self.response, 'type="email"', 1)
+        self.assertContains(self.response, 'type="password"', 2)
 
 class SuccessfulSignUpTests(TestCase):
     def setUp(self):
