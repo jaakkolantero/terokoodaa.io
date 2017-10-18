edf2ffe41861109d31837177a550f7d902016b33
Tero Jaakkola <tero.jaakkola1@gmail.com>
2017-10-18 21:19:56 +0300

    Add __str__ methods to all models

--- boards/models.py
+++ boards/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.auth.models import User
+from django.utils.text import Truncator
+
 
 
 class Board(models.Model):
@@ -19,6 +18,9 @@ class Topic(models.Model):
     board = models.ForeignKey(Board, related_name='topics')
     starter = models.ForeignKey(User, related_name='topics')
 
+    def __str__(self):
+        return self.subject
+
 
 class Post(models.Model):
     message = models.TextField(max_length=4000)
@@ -27,3 +29,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(null=True)
     created_by = models.ForeignKey(User, related_name='posts')
     updated_by = models.ForeignKey(User, null=True, related_name='+')
+
+    def __str__(self):
+        truncated_message = Truncator(self.message)
+        return truncated_message.chars(30)
