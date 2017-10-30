86f56bba57593a691bea0e1aa9a538445c59883b
UFo_x <tero.jaakkola1@gmail.com>
2017-10-30 22:17:39 +0200

    Add email service settings

--- terokoodaa/settings.py
+++ terokoodaa/settings.py
@@ -132,4 +132,12 @@ LOGOUT_REDIRECT_URL = 'board_home'
 LOGIN_REDIRECT_URL = 'board_home'
 LOGIN_URL = 'login'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = 'terokoodaa.io <noreply@terokoodaa.io>'
+EMAIL_SUBJECT_PREFIX = '[terokoodaa.io] '
